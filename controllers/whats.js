e5e3e66610ad285cad057a987fc09a3e5a1f0be4
leinad10 <traviesocontreras3@gmail.com>
2023-03-11 08:23:02 -0500

    dsad

--- controllers/whats.js
+++ controllers/whats.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const Factura = require('../models/factura');
 const bcrypt = require('bcrypt');
 const { model } = require('mongoose');
 const nodemailer = require("nodemailer");
@@ -12,15 +13,15 @@ const client = require('twilio')(process.env.accountSid, process.env.authToken);
     
     
 exports.insertData = (async (request, response) => {
-    const {phone} = request.body
-    client.messages
-    .create({
+    const {id} = request.body
+    const aja = await Factura.find({}, (err, docs) => {
+        console.log(docs);
+    })
+    console.log(aja);
+    client.messages.create({
         body: 'Your appointment is coming up on July 21 at 3PM',
         from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+584242155597'
-    })
-    .then(message => console.log(message.sid))
-    .done();
-    response.status(200).json({ok: message.sid})
+        to: 'whatsapp:+584242155597'}).then(message => console.log(message.sid)).done();
+    response.status(200).json({ok: "mensaje enviado"})
 
   });
